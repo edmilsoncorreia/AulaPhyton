15bd55de7de4357adc0f506419fd72fc317061fd
Edmilson Correia <edmilson.cc@gmail.com>
2024-04-06 05:59:38 -0300

    add aulaOperadores.py

---
+++ aulaOperadores.py
@@ -0,0 +1,18 @@
+# OPERADORES UNÁRIOS
+print("\n-------------------\nOPERADORES UNÁRIOS:\n-------------------\n")
+print("Exemplo número 4")
+print("Operador '~': ", ~4)
+print("Operador '+': ", +4)
+print("Operador '-': ", -4)
+
+# OPERADORES ARITMÉTICOS
+print("\n-----------------------\nOPERADORES ARITMÉTICOS:\n-----------------------\n")
+x = 7
+y = 2
+print(f"Exemplo números {x} e {y}")
+print("Operador '-': ", x - y)
+print("Operador '*': ", x * y)
+print("Operador '**': ", x ** y)
+print("Operador '/': ", x / y)
+print("Operador '//': ", x // y)
+print("Operador '%': ", x % y)
