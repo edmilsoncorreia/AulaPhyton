4e81a29f9fe228d2b1b1c5ba069c48d5b6817cca
Edmilson Correia <edmilson.cc@gmail.com>
2024-04-08 05:55:44 -0300

    add Operadores Relacionais e Lógicos a aulaOperadores.py

--- aulaOperadores.py
+++ aulaOperadores.py
@@ -16,3 +16,20 @@ print("Operador '**': ", x ** y)
 print("Operador '/': ", x / y)
 print("Operador '//': ", x // y)
 print("Operador '%': ", x % y)
+
+#OPERADORES RELACIONAIS
+print("\n-----------------------\nOPERADORES RELACIONAIS:\n-----------------------\n")
+print("Operador '==': ", x == y)
+print("Operador '!=': ", x != y)
+print("Operador '<': ", x < y)
+print("Operador '>': ", x > y)
+print("Operador '>=': ", x >= y)
+print("Operador '<=': ", x <= y)
+
+#OPERADORES LÓGICOS
+print("\n-----------------------\nOPERADORES LÓGICOS:\n-----------------------\n")
+v = True
+f = False
+print("Operador 'and': ", v and f)
+print("Operador 'or': ", v or f)
+print("Operador 'and not': ", v and not f)
