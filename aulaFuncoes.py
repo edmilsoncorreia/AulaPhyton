d8738a92d56660840f22108a93d3aa5953edf42a
Edmilson Correia <edmilson.cc@gmail.com>
2024-04-21 05:42:55 -0300

    add aulaFuncoes.py

---
+++ aulaFuncoes.py
@@ -0,0 +1,64 @@
+#DEFININDO FUNÇÕES
+def PrimeiraFunc():
+    print("Esta é minha primeira função")
+    print("Ela é bem Simples")
+    print("Somente Imprime algumas coisas")
+
+PrimeiraFunc()
+
+print("\n")
+
+def Ola(nome):
+    print("Olá, ",nome)
+    print("Como você vai?")
+
+Ola("Edmilson")
+
+print("\n")
+
+
+#FUNÇÕES COM RETORNO
+print("FUNÇÕES COM RETORNO")
+
+def adicionaDois(numero):
+    numero += 2
+    return numero
+inteiro = 10
+inteiro = adicionaDois(inteiro)
+
+print(inteiro)
+
+print("\n")
+
+#FUNÇÕES COM MÚLTIPLOS ARGUMENTOS
+print("FUNÇÕES COM MÚLTIPLOS ARGUMENTOS")
+
+def Multiplica(n1, n2):
+    return n1*n2
+
+resultado = Multiplica(10, 2)
+print(resultado)
+
+print("\n")
+
+#ARGUMENTOS COM VALOR PADRÃO
+print("ARGUMENTOS COM VALOR PADRÃO")
+
+def Multiplica2(n1 = 3, n2 = 3):
+    return n1*n2
+
+print(Multiplica2())
+print(Multiplica2(20,2))
+
+print("\n")
+
+#NÚMERO VARIÁVEL DE ARGUMENTOS
+print("NÚMERO VARIÁVEL DE ARGUMENTOS")
+
+def multiplicaNumeros(*varArgs):
+    total = 0
+    for arg in varArgs:
+        total *= arg
+    return total
+
+print(multiplicaNumeros(1,2,3,4,5))
